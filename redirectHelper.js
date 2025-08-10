a5eafe278f3bea1c23165f47bd275e003325d6da
Garrett M. <84481668+Garrett-M-code@users.noreply.github.com>
2025-08-10 16:20:33 -0400

    Deleted Resume code
    Deleted the resume code and refactored the important parts.

--- redirectHelper.js
+++
@@ -1,4 +0,0 @@
-
- document.getElementById("resume-button").onclick = function () {
-        location.href = "https://raw.githubusercontent.com/Garrett-M-code/Personal-Website/refs/heads/dev/assets/GARRETT_MOORE_RESUME.pdf";
-    };
