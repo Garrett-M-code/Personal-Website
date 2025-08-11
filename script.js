59f871ad22031bf865f1bdd2caf4cbdfe8016466
Garrett M. <84481668+Garrett-M-code@users.noreply.github.com>
2025-08-11 02:38:00 -0400

    Added Javascript
    Added javascript functionality for button redirects.

---
+++ script.js
@@ -0,0 +1,19 @@
+function downloadResume() {
+  window.location.href = "https://github.com/Garrett-M-code/Personal-Website/blob/352c7fe03c6ff9b7f458a5e89332ad0f3fb11a89/assets/GARRETT_MOORE_RESUME.pdf";
+}
+
+function redirectGithub() {
+  window.location.href = "https://github.com/Garrett-M-code";
+}
+
+function redirectLinkedIn() {
+  window.location.href = "https://www.linkedin.com/in/garrett-moore-/";
+}
+
+function sendEmail() {
+  window.location.href = "mailto:garrettmoorework@gmail.com";
+}
+
+function callPhone() {
+  window.location.href = "tel:+14045903438";
+}
